b4e1dc6bc037b4b1a2f9a0d2b351cb1e89cbf6e9
MLee21 <minnie.lee89@gmail.com>
2016-04-03 21:41:43 -0600

    add angular local storage to persist data.

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic', 'LocalStorageModule'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,4 +21,8 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+app.config(function (localStorageServiceProvider) {
+  localStorageServiceProvider.setPrefix('starter');
+});
