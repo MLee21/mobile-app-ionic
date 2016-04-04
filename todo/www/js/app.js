3e41720f7f162d60e4fd68c0942db5a06f06aacc
MLee21 <minnie.lee89@gmail.com>
2016-04-03 23:17:00 -0600

    complete task function - find task from array using its index, update if it's completed and update Local Storage.

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -58,7 +58,10 @@ app.controller('main', function ($scope, $ionicModal, localStorageService) {
     localStorageService.set(taskData, $scope.tasks);
   }
 
-  $scope.completeTask = function () {
-
+  $scope.completeTask = function (index) {
+    if (index !== -1) {
+      $scope.tasks[index].completed = true;
+    }
+    localStorageService.set(taskData, $scope.tasks);
   }
 });
